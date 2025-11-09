5686aa9136f811cb26481ea9da5322c665c03a38
Odimsom <borrome941@gmail.com>
2025-11-09 15:14:01 -0400

    feat: update basic pages component with stats display and pipe functionality

--- src/app/pages/basic-pages/basic-pages.component.ts
+++ src/app/pages/basic-pages/basic-pages.component.ts
@@ -1,8 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-pages',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-pages.component.html',
 })
-export default class BasicPagesComponent { }
+export default class BasicPagesComponent {
+
+  public nameLower = signal<string>('francisco daniel castro borrome - odimsom');
+  public nameUpper = signal<string>('FRANCISCO DANIEL CASTRO BORROME - ODIMSOM');
+  public fullName = signal<string>('fRaNcIsCo dAnIeL cAsTrO bOrRoMe - oDiMsOm');
+
+
+}
