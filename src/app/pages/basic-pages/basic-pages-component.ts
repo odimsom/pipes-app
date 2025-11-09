6780e8f6dbd1b3fa17aa537d3f941a8ab93aca96
Odimsom <borrome941@gmail.com>
2025-11-09 13:17:30 -0400

    feat: integrate Tailwind CSS and DaisyUI for styling
    - Added Tailwind CSS and DaisyUI dependencies in package.json
    - Configured PostCSS for Tailwind CSS
    - Updated global styles to include Tailwind CSS and DaisyUI themes
    - Removed placeholder content from app.html and replaced with a button
    - Created basic pages with corresponding components and templates
    - Added new components: BasicPages, CustomPage, NumberPage, and UncommonPage
    - Updated VSCode settings for Angular schematics

---
+++ src/app/pages/basic-pages/basic-pages-component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-pages',
+  imports: [],
+  templateUrl: './basic-pages-component.html',
+})
+export default class BasicPagesComponent { }
